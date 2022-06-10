6060556c149881cc7bdc809788d973401d52e341
abdulquddus nuhu <abdulquddusnuhu@gmail.com>
2022-06-10 18:58:29 +0100

    Add project files.

---
+++ ShopifyInventory/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShopifyInventory.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
