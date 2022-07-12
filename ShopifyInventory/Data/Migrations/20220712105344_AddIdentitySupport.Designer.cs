5ce5c3187e12ee4295c85b6afa7e19395e0541a7
abdulquddus nuhu <abdulquddusnuhu@gmail.com>
2022-07-12 11:56:01 +0100

    Add migration for Identity

--- ShopifyInventory/Data/Migrations/20220712105344_AddIdentitySupport.Designer.cs
+++ ShopifyInventory/Data/Migrations/20220712105344_AddIdentitySupport.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ShopifyInventory.Data;
@@ -11,9 +12,10 @@ using ShopifyInventory.Data;
 namespace ShopifyInventory.Migrations
 {
     [DbContext(typeof(ShopifyDbContext))]
-    partial class ShopifyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220712105344_AddIdentitySupport")]
+    partial class AddIdentitySupport
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
